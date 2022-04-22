778b6e25cf0158644c51ae4c383bbf7baad6952f
sudish-K <sudish.k@juegostudioz.com>
2022-04-22 17:11:13 +0530

    initial commit

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,53 +1,54 @@
-const express = require('express')
-const User = require('../models/user')
-const auth = require('../middleware/auth')
-const path = require('path')
-const router = new express.Router()
+const express = require("express");
+const User = require("../models/user");
+const auth = require("../middleware/auth");
+const path = require("path");
+const router = new express.Router();
 
-router.post('/users', async (req, res) => {
-    const user = new User(req.body)
- 
-    try {
-        await user.save()
-        const token = await user.generateAuthToken()
-        res.cookie('auth_token', token)
-        res.sendFile(path.resolve(__dirname, '..', 'views', 'private.html'))
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
 
-router.post('/users/login', async (req, res) => {
-    try {
-        const user = await User.findByCredentials(req.body.email, req.body.password)
-        const token = await user.generateAuthToken()
-        res.cookie('auth_token', token)
-        res.sendFile(path.resolve(__dirname, '..', 'views', 'private.html'))
-    } catch (e) {
-        res.status(400).send()
-    }
-})
+  try {
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.cookie("auth_token", token);
+    res.sendFile(path.resolve(__dirname, "..", "views", "private.html"));
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
-router.post('/users/logout', auth, async (req, res) => {
-    try {
-        req.user.tokens = req.user.tokens.filter((token) => {
-            return token.token !== req.token
-        })
-        await req.user.save()
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.cookie("auth_token", token);
+    res.sendFile(path.resolve(__dirname, "..", "views", "private.html"));
+  } catch (e) {
+    res.status(400).send();
+  }
+});
 
-        res.send()
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+      
+    });
+    await req.user.save();
+    res.sendFile(path.resolve(__dirname, "../../public", "index.html"));
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 
-router.get('/users/profile', auth, async (req, res) => {
-    res.render('profile',{
-        username:req.user.name,
-        email:req.user.email
-    })
-})
+router.get("/users/profile", auth, async (req, res) => {
+  res.render("profile", {
+    username: req.user.name,
+    email: req.user.email,
+  });
+});
 
-
-
-module.exports = router
+module.exports = router;
